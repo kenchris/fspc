88b285aeeae433770094afc133334f0f933ae78d
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2021-12-08 17:34:28 +0100

    Make imports work

--- test/ls.js
+++ test/ls.js
@@ -1,9 +1,15 @@
-export async function main(_, pwd) {
-  for await (const entry of pwd.values()) {
+async function listFiles(handle, indent) {
+  for await (const entry of handle.values()) {
     let output = entry.name;
     if (entry.kind == 'directory') {
       output += '/';
+      const dirHandle = await handle.getDirectoryHandle(entry.name, { create: false });
+      listFiles(dirHandle, indent + "  ");
     }
-    console.log(output);
+    console.log(indent + output);
   }
+}
+
+export async function main(_, pwd) {
+  listFiles(pwd, '');
 }
